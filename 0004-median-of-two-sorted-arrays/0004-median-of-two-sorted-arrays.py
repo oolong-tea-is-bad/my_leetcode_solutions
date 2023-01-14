24aee6ca1ccd4e5c9c529bbaeb54f2528fef9f9e
oolong-tea-is-bad <66204402+oolong-tea-is-bad@users.noreply.github.com>
2023-01-14 22:25:50 +0900

    Time: 71 ms (83.08%), Space: 13.6 MB (78.65%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -9,11 +9,7 @@ class Solution(object):
         res.sort()
         
         if len(res) % 2 == 0:
-            res1 = res[len(res)//2]
-
-            res2 = res[len(res)//2 - 1]
-            res3 = res1 + res2
-            return res3 / 2.0
+            return (res[len(res)//2] + res[len(res)//2 - 1]) / 2.0
         return res[len(res) // 2]
         
         
