ed62826da49cde353473a7b58989e4d5b72175b0
oolong-tea-is-bad <66204402+oolong-tea-is-bad@users.noreply.github.com>
2023-02-06 14:26:07 +0900

    Time: 27 ms (74.27%), Space: 16.4 MB (9.06%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -5,11 +5,59 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    # My Solution / Recursive DFS
     def maxDepth(self, root):
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left)+1, self.maxDepth(root.right)+1)
+    
+    # Recursive BFS
+    def anotherMaxDepth(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
         if not root:
             return 0
-        return max(self.maxDepth(root.left)+1, self.maxDepth(root.right)+1)
+        
+        level = 0
+        q = deque([root])
+        
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            level += 1
+            
+        return level
+    
+    # Iterative DFS
+    def anotherMaxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        stack = [[root, 1]]
+        res = 0
+        
+        while stack:
+            node, depth = stack.pop()
+            
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+                
+        return res
+    
+    
+    
+        
+    
