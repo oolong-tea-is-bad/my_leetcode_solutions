8169e9f442be38633b13eb684e64c118d831facb
oolong-tea-is-bad <66204402+oolong-tea-is-bad@users.noreply.github.com>
2023-02-09 21:40:05 +0900

    Time: 49 ms (50.00%), Space: 16.3 MB (20.03%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        # NeetCode Solution: https://www.youtube.com/watch?v=0K0uCMYq5ng&t=517s&ab_channel=NeetCode
+        
+        def helper(l, r):
+            # Base case where either the left or right subtrees
+            # is iterated completely 
+            if l > r:
+                return None
+            
+            # Midpoint
+            m = (l+r) // 2
+            
+            # Root node defined
+            root = TreeNode(nums[m])
+            # Root's subtrees are made through the recursion
+            root.left = helper(l, m-1)
+            root.right = helper(m+1, r)
+            
+            return root
+            
+        return helper(0, len(nums)-1)
