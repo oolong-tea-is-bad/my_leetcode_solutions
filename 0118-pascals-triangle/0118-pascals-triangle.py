08c99a834cfb44dee74c0505daa4cbb319535934
oolong-tea-is-bad <66204402+oolong-tea-is-bad@users.noreply.github.com>
2023-02-13 13:28:15 +0900

    Time: 15 ms (89.48%), Space: 13.4 MB (43.75%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """   
+        # My Solution
         res = [[1]]
         
         for i in range(numRows-1):
@@ -14,6 +11,18 @@ class Solution(object):
                 else:
                     nextRow.append(res[i][j-1] + res[i][j])
             res.append(nextRow)
+        
+        return res
+    
+        # NeetCode https://www.youtube.com/watch?v=nPVEaB3AjUM&ab_channel=NeetCode
+        res = [[1]]
+        
+        for i in range(numRows-1):
+            temp = [0] + [res-1] + [0]
+            row = []
+            for j in range(len(res-1)+1):
+                row.append(temp[j]+[j+1])
+            res.append(row)
         
         return res
             
