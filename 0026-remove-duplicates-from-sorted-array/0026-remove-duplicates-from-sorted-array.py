8369d800d96272e3d95a62e749447ba21ee7a895
oolong-tea-is-bad <66204402+oolong-tea-is-bad@users.noreply.github.com>
2023-05-09 21:44:40 +0900

    Time: 107 ms (25.01%), Space: 18 MB (9.75%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        numDic = {nums[0]:1}
+        k = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] not in numDic:
+                numDic[nums[i]] = 1
+                k += 1
+                
+                nums[k-1] = nums[i]
+            else:
+                numDic[nums[i]] += 1 
+
+        return k
+        
+            
