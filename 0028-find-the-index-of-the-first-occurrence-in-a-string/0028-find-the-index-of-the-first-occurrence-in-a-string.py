a432d0a35b501f36a106775ba242575a5af9b640
oolong-tea-is-bad <66204402+oolong-tea-is-bad@users.noreply.github.com>
2023-05-10 23:00:18 +0900

    Time: 41 ms (16.62%), Space: 16.3 MB (9.80%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,6 +1,9 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        # needle의 길이 만큼의 섹션을 haystack에서 계속 검색할거임
         for i in range(len(haystack) - len(needle) + 1):
+            
+            # 그 섹션이 needle과 같다면
             if haystack[i:i+len(needle)] == needle:
                 return i
 
