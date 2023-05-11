51f5848c7f5329567c2b0145e1574ad2621fbe22
oolong-tea-is-bad <66204402+oolong-tea-is-bad@users.noreply.github.com>
2023-05-11 21:55:28 +0900

    Time: 51 ms (14.02%), Space: 18.8 MB (15.24%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -8,12 +8,14 @@ class Solution:
         headDic = {}
         index = 0
         
+        # 딕셔너리에 index+1을 key, 그 node의 값을 value
         while head:
             index += 1
             
             headDic[index] = head.val
             head = head.next
             
+        # 일반적인 linked list 만드는 과정
         header = ListNode(None)
         starter = header
         
